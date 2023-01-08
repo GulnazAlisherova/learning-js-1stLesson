ddd771ba31b577641f5f290422653ac01df13a5d
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-01-08 16:39:31 +0600

    added new assignment and added comment

--- script.js
+++ script.js
@@ -25,16 +25,18 @@ var names = firstName + lastName;
 console.log(names);
 
 //math-3
-var result = (2 + 1) / 3 - 6 * 2;
+result = (2 + 1) / 3 - 6 * 2;
+// result = 5 - 1 - 10 * 3; //4-30
 console.log(result);
 
+//math 4
 var a = 5;
 var b = 6;
 var squareArea = a * b;
 // alert(squareArea)
 console.log(squareArea)
 
-//example 3
+//example 5
 var PI = 3.141592;
 var r = 5;
 var circleArea = r * r * PI;
