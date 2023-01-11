724bf1bacca48bf7ca5ad69631ebc7f11ea556cb
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-01-11 12:12:20 +0600

    added new examples for second JS document

---
+++ script2.js
@@ -0,0 +1,15 @@
+// var example1 = "Hello " + "world!";
+// console.log(example1);
+
+// // var example1 = "Hello " - "world!";
+// // console.log(example1);   минус нельзя использовать, только ПЛЮС!!
+
+// var example2 = "200" + 700;
+// console.log(example2);
+
+// var example3 = "10" + "10";
+// console.log(example3);
+
+var age = prompt("How old are you?");
+var year = 2023 - age;
+console.log(year);
