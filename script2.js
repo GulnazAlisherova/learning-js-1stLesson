5a13a2021bd68f2c3a3d570a504fc8fedea90016
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-01-13 13:23:22 +0600

    added new HTML tags inside -> document.writeln

--- script2.js
+++ script2.js
@@ -117,6 +117,7 @@ function calculateCylinderVolume(r, h) { // ёмкость цилиндра
 // console.log(bottle);
 
 document.write("Hello ");
-document.write("world ");
+document.write("world <br> ");
 
-
+document.writeln("<h1> bye world! </h1> <br>");
+document.writeln("<h3> goodbye world </h3><br>");
