c58f5ece47d80e4c7de31e17e111a3ebb0e04b26
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-01-14 13:13:49 +0600

    added two document.writes with typeof inside

--- script3.js
+++ script3.js
@@ -2,13 +2,20 @@ var emptyVariable;
 var message = "Hello world"; //most usable
 var firstName, lastName = "Ulanov";
 
+
+// function deleteNumber(a, b){
+//   console.log(a / b);
+// }
+// deleteNumber(1, 0);
+
+
 //numbers -> data type
+//JS - математические операции не ведут к ошибке  example NAN or INfinity
 var exampleNumber1 = 123;
 var exampleNumber2 = 123.45;
-var exampleNumber3 = Infinity; // 1 / 0 -infinity
-var exampleNumber4 = Nan;
+// var exampleNumber3 = Infinity; // 1 / 0 -infinity
+// var exampleNumber4 = Nan; // not a number 
 
-function deleteNumber(a, b){
-  console.log(a / b);
-}
-deleteNumber(1, 0);
+document.write(typeof "nature");
+
+document.write(typeof exampleNumber1);
