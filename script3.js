6bed1171f4e7abe2ac9e7a1d419461a18814361a
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-01-14 13:00:39 +0600

    added fourth example about number data type

--- script3.js
+++ script3.js
@@ -5,7 +5,8 @@ var firstName, lastName = "Ulanov";
 //numbers -> data type
 var exampleNumber1 = 123;
 var exampleNumber2 = 123.45;
-var exampleNumber3 = Infinity; // 1 / 0
+var exampleNumber3 = Infinity; // 1 / 0 -infinity
+var exampleNumber4 = Nan;
 
 function deleteNumber(a, b){
   console.log(a / b);
