f8084d5fb9b7eba5f092f34b0f9341893321691e
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-01-14 12:56:57 +0600

    added new variables about number data types

--- script3.js
+++ script3.js
@@ -2,3 +2,7 @@ var emptyVariable;
 var message = "Hello world"; //most usable
 var firstName, lastName = "Ulanov";
 
+//numbers -> data type
+var exampleNumber1 = 123;
+var exampleNumber2 = 123.45;
+var exampleNumber3 = Infinity; // 1 / 0
