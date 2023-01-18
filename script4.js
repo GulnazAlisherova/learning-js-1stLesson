3b32a572315ead03f33ab24354e16509fbe85cbe
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-01-18 21:25:20 +0600

    optimizied method getelementbyid

--- script4.js
+++ script4.js
@@ -1,6 +1,14 @@
 // document.body.style.background = "lightblue";
 // document.body.textContent = "Hello guys";
 
-document.getElementById("hello-world").textContent = "Nice to meet ya";
-document.getElementById("hello-world").style.background = "blue";
-document.getElementById("hello-world").style.color = "white";
+// document.getElementById("hello-world").textContent = "Nice to meet ya";
+// document.getElementById("hello-world").style.background = "blue";
+// document.getElementById("hello-world").style.color = "white";
+
+//optimizition
+
+let helloWorldDiv = document.getElementById("hello-world");
+
+helloWorldDiv.textContent = "Nice to meet you!";
+helloWorldDiv.style.color= "white"
+helloWorldDiv.style.background= "green"
