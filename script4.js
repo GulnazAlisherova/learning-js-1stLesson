2414fec6190e4df8e7000ffb298795eefc1fdecc
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-01-18 21:32:09 +0600

    added comment for last font-size

--- script4.js
+++ script4.js
@@ -13,4 +13,4 @@ helloWorldDiv.textContent = "Nice to meet you!";
 helloWorldDiv.style.color= "white";
 helloWorldDiv.style.background= "green";
 helloWorldDiv.style.textDecoration= "underline";
-helloWorldDiv.style.fontSize= "20px";
+helloWorldDiv.style.fontSize= "20px"; // numbers should write in ""
