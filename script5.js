a308728d73e01a82599202b9c69f7a826649eb12
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-01-24 20:09:44 +0600

    on click for buttons added new example with several flexBasis sizes

--- script5.js
+++ script5.js
@@ -8,11 +8,17 @@ let button2 = document.querySelector('button:nth-of-type(2)');
 let button3 = document.querySelector('button:nth-of-type(3)');
 
 button1.addEventListener('click', function(){
-
+  for (let p of allP) {
+    p.style.flexBasis = "90%";
+  }
 });
 button2.addEventListener('click', function(){
-
+  for (let p of allP) {
+    p.style.flexBasis = "45%";
+  }
 });
 button3.addEventListener('click', function(){
-
+  for (let p of allP) {
+    p.style.flexBasis = "30%";
+  }
 });
