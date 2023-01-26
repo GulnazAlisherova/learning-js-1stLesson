70d462018bc6d6479885c210668af5863f31b7de
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-01-26 23:08:58 +0600

    commented out

--- script5.js
+++ script5.js
@@ -1,25 +1,27 @@
-let allP = document.querySelectorAll('p');
-for (let p of allP) {
-  p.style.flexBasis = "45%";
-}
+// let allP = document.querySelectorAll('p');
+// for (let p of allP) {
+//   p.style.flexBasis = "45%";
+// }
 
-let button1 = document.querySelector('button:nth-of-type(1)');
-let button2 = document.querySelector('button:nth-of-type(2)');
-let button3 = document.querySelector('button:nth-of-type(3)');
+// let button1 = document.querySelector('button:nth-of-type(1)');
+// let button2 = document.querySelector('button:nth-of-type(2)');
+// let button3 = document.querySelector('button:nth-of-type(3)');
 
-button1.addEventListener('click', function(){
-  for (let p of allP) {
-    // p.style.flexBasis = "90%";
-  }
-  document.write("Hello world");
-});
-button2.addEventListener('click', function(){
-  for (let p of allP) {
-    p.style.flexBasis = "45%";
-  }
-});
-button3.addEventListener('click', function(){
-  for (let p of allP) {
-    p.style.flexBasis = "30%";
-  }
-});
+// button1.addEventListener('click', function(){
+//   for (let p of allP) {
+//     // p.style.flexBasis = "90%";
+//   }
+//   document.write("Hello world");
+// });
+// button2.addEventListener('click', function(){
+//   for (let p of allP) {
+//     p.style.flexBasis = "45%";
+//   }
+// });
+// button3.addEventListener('click', function(){
+//   for (let p of allP) {
+//     p.style.flexBasis = "30%";
+//   }
+// });
+
+// как модифицировать документ
